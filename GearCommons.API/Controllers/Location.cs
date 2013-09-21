5a13e3d6010240dd519db4649d6ee2f29b684299
Tim Burkhart <timmyb_84@hotmail.com>
2013-09-21 13:01:38 -0400

    stuff

--- GearCommons.API/Controllers/Location.cs
+++ GearCommons.API/Controllers/Location.cs
@@ -1,8 +1,11 @@
-﻿namespace GearCommons.API.Controllers {
+﻿using System;
+
+namespace GearCommons.API.Controllers {
 	public class Location {
 		public double Latitude { get; set; }
 		public string LatitudeDirection { get; set; }
 		public double Longitude { get; set; }
 		public string LongitudeDirection { get; set; }
+		public Guid Id { get; set; }
 	}
 }
--- GearCommons.API/Controllers/LocationsController.cs
+++ GearCommons.API/Controllers/LocationsController.cs
@@ -1,4 +1,5 @@
-﻿using System.Web.Helpers;
+﻿using System;
+using System.Web.Helpers;
 using System.Web.Http;
 using Newtonsoft.Json;
 
@@ -7,20 +8,20 @@ namespace GearCommons.API.Controllers {
 		// GET api/locations
 		public dynamic Get() {
 			var locations = new[] {
-				new Location {Latitude = 42.3581, LatitudeDirection = "N", Longitude = 71.0636, LongitudeDirection = "W"}, new Location {Latitude = 52.2050, LatitudeDirection = "N", Longitude = 0.1190, LongitudeDirection = "E"}
+				new Location {Latitude = 42.3581, LatitudeDirection = "N", Longitude = 71.0636, LongitudeDirection = "W", Id = new Guid("C4F4ADEF-641C-4098-9BB2-E13AB44DA9C7")}, 
+				new Location {Latitude = 52.2050, LatitudeDirection = "N", Longitude = 0.1190, LongitudeDirection = "E", Id = new Guid("655904A9-DAB1-4098-94B2-37F3BA0AF227")}
 			};
 			return Json.Encode(locations);
 		}
 
 		// GET api/values/5
-		public dynamic Get(int id) {
-			//var asdf = new Blah { Hmm = "Ok"};
-			//return Json.Encode(asdf);
-			return null;
+		public dynamic Get(Guid id) {
+			return new Location {Latitude = 42.3581, LatitudeDirection = "N", Longitude = 71.0636, LongitudeDirection = "W", Id = new Guid("C4F4ADEF-641C-4098-9BB2-E13AB44DA9C7")};
 		}
 
 		// POST api/values
 		public void Post([FromBody]string value) {
+
 		}
 
 		// PUT api/values/5
