a0d599a787c12e4ce3cd86803a40a914d8156b38
Tim Burkhart <timmyb_84@hotmail.com>
2013-09-21 12:48:22 -0400

    Added locations

---
+++ GearCommons.API/Controllers/Location.cs
@@ -0,0 +1,8 @@
+﻿namespace GearCommons.API.Controllers {
+	public class Location {
+		public double Latitude { get; set; }
+		public string LatitudeDirection { get; set; }
+		public double Longitude { get; set; }
+		public string LongitudeDirection { get; set; }
+	}
+}
---
+++ GearCommons.API/Controllers/LocationsController.cs
@@ -0,0 +1,32 @@
+﻿using System.Web.Http;
+
+namespace GearCommons.API.Controllers {
+	public class LocationsController : ApiController {
+		// GET api/locations
+		public dynamic Get() {
+			return new[] {
+				new Location { Latitude = 42.3581, LatitudeDirection = "N", Longitude = 71.0636, LongitudeDirection = "W"},
+				new Location { Latitude = 52.2050, LatitudeDirection = "N", Longitude = 0.1190, LongitudeDirection = "E"}
+			};
+		}
+
+		// GET api/values/5
+		public dynamic Get(int id) {
+			//var asdf = new Blah { Hmm = "Ok"};
+			//return Json.Encode(asdf);
+			return null;
+		}
+
+		// POST api/values
+		public void Post([FromBody]string value) {
+		}
+
+		// PUT api/values/5
+		public void Put(int id, [FromBody]string value) {
+		}
+
+		// DELETE api/values/5
+		public void Delete(int id) {
+		}
+	}
+}
