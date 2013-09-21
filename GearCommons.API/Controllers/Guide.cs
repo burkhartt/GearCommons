0c02d1b8f339e7a43f99597174042b13e80bf5f3
Tim Burkhart <timmyb_84@hotmail.com>
2013-09-21 15:17:16 -0400

    asdf

--- GearCommons.API/Controllers/Guide.cs
+++ GearCommons.API/Controllers/Guide.cs
@@ -5,9 +5,7 @@ using System.Linq;
 namespace GearCommons.API.Controllers {
 	public class Guide {
 		public double Latitude { get; set; }
-		public string LatitudeDirection { get; set; }
 		public double Longitude { get; set; }
-		public string LongitudeDirection { get; set; }
 		public Guid Id { get; set; }
 		public string Name { get; set; }
 		public string Description { get; set; }
